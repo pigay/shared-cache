d9cf7d727dd1a7a2e03bd75b436b1e48f243794d
Pierre Gay <peyo@libvsg.org>
2016-03-17 10:29:52 +0100

    bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup( name = 'shared-cache',
-      version = '0.1',
+      version = '0.2.0',
       description = 'multiple process shared-cache tool',
       author = 'Pierre Gay',
       author_email = 'pierre.gay@u-bordeaux.fr',
